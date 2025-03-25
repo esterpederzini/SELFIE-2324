8e3edb90fb483543e8d3fc3c2ce093d2c8322774
Fiammetta21 <fiamm@Fiammettagit config --global user.name Fiammetta21git commit -m indexgit cleargit config --global user.email fiamm@Fiammetta>
2025-03-25 16:35:22 +0100

    login

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,7 +1,78 @@
-export default function Login(){
-    return (
-        <div className='Login'>
-            <h1>Login view</h1>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import "../CSS/login.css"; // Collegamento al file CSS
+import Sfondologin from "../Img/Sfondo login.jpg"
+//import { IonIcon } from "react-ionicons"; // Per le icone Ionicons
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login attempt:", formData);
+  };
+
+  return (
+    <section>
+      <div className="login-box">
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
+
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="person"></ion-icon>
+            </span>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+            <label>Username</label>
+          </div>
+
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="lock-closed"></ion-icon>
+            </span>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <label>Password</label>
+          </div>
+
+          <button type="submit">Login</button>
+
+          <div className="register-link">
+            <p>
+              Don't have an account? <a href="/signup">Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
+
+      {/* Inclusione degli script Ionicons per le icone */}
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        nomodule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+      ></script>
+    </section>
+  );
+};
+
+export default Login;
